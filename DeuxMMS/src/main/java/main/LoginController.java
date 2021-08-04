d1c4350331900411ffabc1dd993de17fae064a86
jlstas <56229045+jlstas@users.noreply.github.com>
2021-08-04 18:16:42 -0400

    Rearranged Files to make html pages work on server

--- DeuxMMS/src/main/java/main/LoginController.java
+++ DeuxMMS/src/main/java/main/LoginController.java
@@ -10,8 +10,26 @@ package main;
  * @author Jess
  */
 public class LoginController {
+    
+    private String cMessage = "Successful Login";
+    private String uMessage = "Invalid Credentials";
+    User user = new User();
+    private boolean confirm = false;
     //constructor
     public LoginController(){
         
+    }
+    
+    private boolean authenticateLogin(User user){
+        this.user = user;
+        return confirm;
+    }
+    
+    public String loginUnsuccessful(){
+        return this.uMessage;
+    }
+    
+    public String confirmLogin(){
+        return this.cMessage;
     }
 }
